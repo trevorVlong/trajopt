43d9bf5cb7ff91d3c6b9c7f6ad4009e682d63b0f
trevorVlong <longtrevorv@gmail.com>
2025-11-24 17:18:59 -0500

    courtin 2d surrogates working

--- trajopt/aerodynamics/courtinSurrogates.py
+++ trajopt/aerodynamics/courtinSurrogates.py
@@ -33,34 +33,31 @@ def liftCoeff(alfa,delta_f,delta_cj):
     """
 
     # parametrs given
-    c1 = 0.5393
-    c2 = 0.0281
-    c3 = 0.0262
-    c4 = 0.0711
-    c5 = 0.0036
-    c6 = 0.0031
-    c7 = -0.005
-    c8 = 0.035
-    c9 = 0.010
-    c10 = 0.0053
-    c11 = 0.0264
-    c12 = 0.1712
+    c1 = 0.1856
+    c2 = 0.0334
+    c3 = 0.0667
+    c4 = 0.0121
+    c5 = -0.0085
+    c6 = 0.0426
+    c7 = 0.0140
+    c8 = 0.0226
+    c9 = 0.1407
 
     # write out func in 3 parts to make easier to read
 
     # const part w.r.t. cj
-    f1 =c1 + (c2+c3)*delta_f + c4*alfa
+    f1 = c1 + c2*delta_f + c3*alfa
 
     # square root part w.r.t. cj
-    f2 = c5 + (c6+c7)*delta_f + c8*alfa
+    f2 = delta_cj ** 0.5 * (c4 * delta_f + c5 * alfa + c7)
 
     # linear part w.r.t. cj
-    f3 = c9 + (c10+c11)*delta_f + c12*alfa
+    f3 = delta_cj * (c6*delta_f + c8*alfa + c9)
 
-    return f1 + np.sqrt(delta_cj)*f2 + delta_cj*f3
+    return (f1+f2+f3)
 
 
-def pitchingCoeff(alfa, delta_f, delta_cj):
+def pitchingCoeff(alfa, delta_f, delta_cj,kcm=1):
     """
     pitching coefficient surrogate model fit taking the form given by courtin in E.23->E.29. makes simplification that
     j1,j2 are equal and that both are the flap angle delta_f
@@ -71,34 +68,30 @@ def pitchingCoeff(alfa, delta_f, delta_cj):
     """
 
     # parametrs given
-    c1 = -0.1135
-    c2 = -0.0033
-    c3 = -0.0029
-    c4 = 0.0050
-    c5 = 2.927e-4
-    c6 = 2.302e-4
-    c7 = 0.0012
-    c8 = 0.1066
-    c9 = -0.0028
-    c10 = -0.0018
-    c11 = 0.0049
-    c12 = -0.2769
-
+    c1 = -0.128
+    c2 = -0.0114
+    c3 = -0.0387
+    c4 = -0.0143
+    c5 = 0.0278
+    c6 = 0.0174
+    c7 = -0.0120
+    c8 = -0.0017
+    c9 =  0.0238
     # write out func in 3 parts to make easier to read
 
     # const part w.r.t. cj
-    f1 = c1 + (c2 + c3) * delta_f + c4 * alfa
+    f1 = c1 + c2*delta_f + c3*alfa
 
     # square root part w.r.t. cj
-    f2 = c5 + (c6 + c7) * delta_f + c8 * alfa
+    f2 = delta_cj ** 0.5 * (c4 * delta_f + c5 * alfa + c6)
 
     # linear part w.r.t. cj
-    f3 = c9 + (c10 + c11) * delta_f + c12 * alfa
+    f3 = delta_cj * (c7*delta_f + c8*alfa + c9)
 
-    return f1 + np.sqrt(delta_cj) * f2 + delta_cj * f3
+    return kcm * (f1+f2+f3)
 
 
-def dragCoeff(CL,dcj,AR,Tc):
+def dragCoeff(CL,dcj,AR):
     """
     drag coefficient estimate as a sum of a constant zero-lift drag (CD0) and a induced drag component. Negative
     implies net thrust while positive implies net drag
@@ -111,13 +104,13 @@ def dragCoeff(CL,dcj,AR,Tc):
 
     # set constant drag, not sure what to use for this except make it "reasonable"
 
-    CD0 = 1
+    CD0 = 0.5
 
     # induced drag component
 
-    CDi = CL**2 / (np.pi * AR * 0.35 + 2*dcj)
+    CDi = CL**2 / (np.pi * AR *0.426 + 2*dcj)
 
-    return CD0 + CDi - Tc
+    return CD0 + CDi
 
 
 if __name__ == "__main__":
@@ -127,37 +120,48 @@ if __name__ == "__main__":
     c = 0.38
     bt = 1.27
     ct = 0.25
-    AR = b/c
+    Sref = 1.09
+    AR = b**2/Sref
 
     # blowing / other params
     E = 0.3  # flap chord fraction
-    Adisk = .065
-    h = 0.032
+    Adisk = 10 *.0061
+    h = 0.0375
     c = 0.38
     rho = 1.225
 
     # problem params
     Vinf = 10
-    T = np.linspace(0,90,100)
-    Tc = T/(0.5*rho*Vinf**2 * Adisk)
-    dcj = deltaJetCoeffFromThrust(T,Adisk,Vinf,h,c,rho)
-    alfa = -1
-    beta = 30
+    T = np.linspace(0,50,5)
+
+    alfas = np.linspace(-5,30,100)
+    flapang = 20
 
     # plot lift coefficient as function of cj
-
-    cl = liftCoeff(alfa,beta,dcj)
-    cd = dragCoeff(cl,dcj,AR,Tc)
-    cm = pitchingCoeff(alfa,beta,dcj)
-
     fig, axs = plt.subplots(2, 2)
-    axs[0, 0].plot(dcj, cl)
-    axs[0, 0].set_title('Axis [0, 0]')
-    axs[0, 1].plot(dcj, cd, 'tab:orange')
-    axs[0, 1].set_title('Axis [0, 1]')
-    axs[1, 0].plot(dcj, cm, 'tab:green')
-    axs[1, 0].set_title('Axis [1, 0]')
-    axs[1, 1].plot(cl, cm, 'tab:red')
-    axs[1, 1].set_title('Axis [1, 1]')
+    for Tidx in T:
+        vrat = np.sqrt(Tidx / (0.5 * rho * Vinf ** 2 * Adisk) + 1)
+        dcj = Adisk/Sref * (vrat**2-1)*(vrat**2 + 1)
+        cl = liftCoeff(alfas,flapang,dcj)
+        cd = dragCoeff(cl,dcj,AR) - Tidx/(0.5*rho*Vinf**2*Adisk)
+        cm = pitchingCoeff(alfas,flapang,dcj)
+        print(f"{dcj}")
 
+        axs[0, 0].plot(alfas, cl)
+
+        axs[0, 1].plot(alfas, cd)
+        axs[1, 0].plot(alfas, cm)
+        axs[1, 1].plot(cl, cm)
+    axs[0, 0].grid()
+    axs[0, 1].grid()
+    axs[1, 1].grid()
+    axs[1, 0].grid()
+    axs[0, 0].set_ylabel('cl')
+    axs[0, 0].set_xlabel('alfa')
+    axs[0, 1].set_ylabel('cd')
+    axs[0, 1].set_xlabel('alfa')
+    axs[1, 0].set_ylabel('cm')
+    axs[1, 0].set_xlabel('alfa')
+    axs[1, 1].set_ylabel('cm')
+    axs[1, 1].set_xlabel('cl')
     plt.show()
